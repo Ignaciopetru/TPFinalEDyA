7f2086d3b2a11ba5b249ce539332edaed3bcdacb
petru <petruignacio@gmail.com>
2020-07-15 09:52:44 -0300

    Interfaz seguimos

--- main.c
+++ main.c
@@ -36,6 +36,7 @@ int es_un_numero_con_caracter(char *palabra, char caracter) {
   return 1;
 }
 
+// si el alias ya esta almacenado devulve 1, sino no esta almacenado 0, si es erroneo sintacticamente 2
 int alias_validar (char *alias) {
   for(int i = 0; alias[i]!= '\0'; i++) {
     if(isalpha(alias[i]) == 0)
@@ -46,7 +47,7 @@ int alias_validar (char *alias) {
 
 Token *token_crear (char *palabra) {
   Token *token = malloc(sizeof(Token));
-  token->alias = calloc(200,sizeof(char));
+  token->alias = calloc(CAPACIDAD,sizeof(char));
   if (strcmp(palabra, "salir") == 0) {
     token->tipo = 1;
     return token;
@@ -78,7 +79,7 @@ Token *token_crear (char *palabra) {
   } else if (strcmp(palabra, "x") == 0) {
     token->tipo = 10;
     return token;
-  } else if (palabra[strlen(palabra)-1] == '}'&& es_un_numero_con_caracter(palabra, '}')) {
+  } else if (palabra[strlen(palabra)-1] == '}' && es_un_numero_con_caracter(palabra, '}')) {
     token->tipo = 11;
     token->alias = strcpy(token->alias, palabra);
     token->alias[strlen(palabra)-1] = '\0';
@@ -111,7 +112,7 @@ Token *token_crear (char *palabra) {
 
 Tokens token_lista_crear (char *comando) {
   Tokens lista;
-  lista.palabras = calloc(500, sizeof(Token*));
+  lista.palabras = calloc(CAPACIDAD, sizeof(Token*));
   char *palabra = strtok(comando, " ");
   int i = 0;
    while( palabra != NULL ) {
@@ -132,6 +133,9 @@ void tokens_destruir(Tokens lista) {
 }
 
 int parser(HashTabla *tabla, Tokens lista) {
+
+    if(lista.largo == 0)
+      return 1;
 
     if (lista.largo == 1) {
       if (lista.palabras[0]->tipo == 1){
@@ -161,9 +165,9 @@ int parser(HashTabla *tabla, Tokens lista) {
 
     if (lista.largo == 3) {
       if (lista.palabras[1]->tipo == 3 && lista.palabras[0]->tipo == 15) {
-        if (alias_validar(lista.palabras[0]->alias)) {
+        if (alias_validar(lista.palabras[0]->alias)) { // no tiene por que existir
           if (lista.palabras[2]->tipo == 7) {
-            if (alias_validar(lista.palabras[2]->alias)) {
+            if (alias_validar(lista.palabras[2]->alias)) { // tiene que existir
               // guardar complemento del 2do en el 1ro
               printf("Complemento\n");
               return 1;
@@ -195,7 +199,7 @@ int parser(HashTabla *tabla, Tokens lista) {
 
     if (lista.largo == 5) {
       if (lista.palabras[0]->tipo == 15 && lista.palabras[1]->tipo == 3 && lista.palabras[2]->tipo == 15 && lista.palabras[3]->tipo > 3 && lista.palabras[3]->tipo < 7 && lista.palabras[4]->tipo == 15) {
-        if (alias_validar(lista.palabras[0]->alias) && alias_validar(lista.palabras[2]->alias) && alias_validar(lista.palabras[4]->alias)) {
+        if (alias_validar(lista.palabras[0]->alias) && alias_validar(lista.palabras[2]->alias) && alias_validar(lista.palabras[4]->alias)) { // 1er puedo no existir los otros dos si o si.
           switch (lista.palabras[3]->tipo) {
           case 4:{
             printf("Hacer union\n");
@@ -221,8 +225,8 @@ int parser(HashTabla *tabla, Tokens lista) {
 
     if (lista.largo == 8) {
       if (lista.palabras[0]->tipo == 15 && lista.palabras[1]->tipo == 3 && lista.palabras[2]->tipo == 8 && lista.palabras[3]->tipo == 13 && lista.palabras[4]->tipo == 9 && lista.palabras[5]->tipo == 10 && lista.palabras[6]->tipo == 9 && lista.palabras[7]->tipo == 11) {
-        if (alias_validar(lista.palabras[0]->alias)) {
-          printf("Crear conjunto: {%s, %s}", lista.palabras[3]->alias, lista.palabras[7]->alias);
+        if (alias_validar(lista.palabras[0]->alias)) { // no tiene porque exisitir
+          printf("Crear conjunto: {%s, %s}", lista.palabras[3]->alias, lista.palabras[7]->alias); // verificar valides de intervalo
           return 1;
         } else {
           printf("Alias invalido\n");
@@ -231,8 +235,8 @@ int parser(HashTabla *tabla, Tokens lista) {
       }
     }
 
-    if (lista.palabras[0]->tipo == 15) {
-      if (alias_validar(lista.palabras[0]->alias)) {
+    if (lista.palabras[0]->tipo == 15 && lista.largo > 2) {
+      if (alias_validar(lista.palabras[0]->alias)) { // no tiene
         printf("Crar conjunto\n");
         for (int i = 2; i < lista.largo; i++) {
           if (lista.palabras[i]->tipo == 11 ||lista.palabras[i]->tipo == 12 || lista.palabras[i]->tipo == 14) {
@@ -251,12 +255,14 @@ int parser(HashTabla *tabla, Tokens lista) {
       }
 
     }
+    printf("Comando invalido\n");
+    return 1;
 
 }
 
 int main() {
-  char ff[200] = "asd' = {123, 2, 3}";
-  fgets (ff, 200, stdin);
+  char ff[CAPACIDAD] = "asd' = {123, 2, 3}";
+  fgets (ff, CAPACIDAD, stdin);
   ff[strlen(ff)-1] = '\0';
   Tokens hola = token_lista_crear(ff);
   for(int i = 0; i<hola.largo; i++) {
